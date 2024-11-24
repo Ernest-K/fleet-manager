c0284adf880a88ef26ca373d4c1796a0420df224
Ernest K <ernio789@gmail.com>
2024-11-24 14:37:22 +0100

    add email reminders to cron config

--- src/features/drivers/hooks/useGetDrivers.ts
+++ src/features/drivers/hooks/useGetDrivers.ts
@@ -2,13 +2,15 @@ import { useQuery } from "@tanstack/react-query";
 import { collection, query, where, getDocs } from "firebase/firestore";
 import { db } from "@/../firebase";
 import { Driver } from "@/features/drivers/types";
+import { CollectionNames } from "@/types";
+import { Role } from "@/features/auth/types";
 
 type GetDriversOptions = {
   managerUid: string;
 };
 
 export const getDrivers = async ({ managerUid }: GetDriversOptions): Promise<Driver[]> => {
-  const driverQuery = query(collection(db, "users"), where("role", "==", "driver"), where("managerUid", "==", managerUid));
+  const driverQuery = query(collection(db, CollectionNames.Users), where("role", "==", Role.Driver), where("managerUid", "==", managerUid));
   const querySnapshot = await getDocs(driverQuery);
   return querySnapshot.docs.map((doc) => doc.data() as Driver);
 };
